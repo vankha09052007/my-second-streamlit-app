7dc0858a197780dc80e019fe7c9710a47360950b
vankha09052007 <127587432+vankha09052007@users.noreply.github.com>
2023-03-17 23:28:55 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import sklearn
+model = pickle.load(open("model.pickle", "rb"))
+
+st.title('Revenue Prediction')
+temp = st.number_input('Input Temperature')
+temp= np.array([temp]).reshape(-1,1)
+kq = model.predict(temp)[0]
+
+if st.button("Predict"):
+  st.write("Revenue Prediction")
+  st.write(kq)
